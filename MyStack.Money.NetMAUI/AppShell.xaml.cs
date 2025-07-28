2f5d14ab9e7bc75510f60f74f43f8fe595e76706
Pablo Orue <pablo.orue@educacionit.com>
2025-07-28 18:46:03 -0300

    MyStack.Money.NetMAUI

---
+++ MyStack.Money.NetMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyStack.Money.NetMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyStack.Money.NetMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyStack.Money.NetMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
