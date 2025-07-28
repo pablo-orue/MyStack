74089d46b7216e49b7c8c54966e848b17a08a0fa
Pablo Orue <pablo.orue@educacionit.com>
2025-07-28 17:03:35 -0300

    TradeService

---
+++ MyStack.Money.Service/ITradeService.cs
@@ -0,0 +1,13 @@
+﻿using MyStack.Money.Domain;
+
+namespace MyStack.Money.Service
+{
+    public interface ITradeService
+    {
+        Task<IEnumerable<Trade>> GetAllAsync();
+        Task<Trade?> GetByIdAsync(int id);
+        Task<Trade> CreateAsync(Trade trade);
+        Task<bool> UpdateAsync(Trade trade);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ MyStack.Money.Service/TradeService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyStack.Infrastructure.EF;
+using MyStack.Money.Domain;
+
+namespace MyStack.Money.Service
+{
+    internal class TradeService : ITradeService
+    {
+        private readonly OrueContext _context;
+
+        public TradeService(OrueContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Trade>> GetAllAsync()
+        {
+            return await _context.Trades.ToListAsync();
+        }
+
+        public async Task<Trade?> GetByIdAsync(int id)
+        {
+            return await _context.Trades.FindAsync(id);
+        }
+
+        public async Task<Trade> CreateAsync(Trade trade)
+        {
+            _context.Trades.Add(trade);
+            await _context.SaveChangesAsync();
+            return trade;
+        }
+
+        public async Task<bool> UpdateAsync(Trade trade)
+        {
+            var existing = await _context.Trades.FindAsync(trade.Id);
+            if (existing is null)
+            {
+                return false;
+            }
+            _context.Entry(existing).CurrentValues.SetValues(trade);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var trade = await _context.Trades.FindAsync(id);
+            if (trade is null)
+            {
+                return false;
+            }
+            _context.Trades.Remove(trade);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
